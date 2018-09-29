77257c5813154ed336ab4a94ca35a649aac059d1
zhanglong <541217364@qq.com>
2018-09-29 16:36:13 +0800

    Initial Commit

---
+++ Adver-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Adver-Demo
+//
+//  Created by 周启磊 on 2018/9/29.
+//  Copyright © 2018年 DIDIWAIMAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Adver-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Adver-Demo
+//
+//  Created by 周启磊 on 2018/9/29.
+//  Copyright © 2018年 DIDIWAIMAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
